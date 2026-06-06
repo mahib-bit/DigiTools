0f231fafea04d245d072a3ef9f00f47540e8bc00
mahib-bit <sadmanmahib@gmail.com>
2026-06-06 22:07:40 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+import Banner from './Components/Banner/Banner'
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar />
+      <Banner />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+import 'react';
+
+const Navbar = () => {
+    return (
+        <div className="flex navbar bg-base-100 shadow-lg justify-between items-center px-10 py-5 max-w-[1500px] mx-auto">
+            <a className="text-3xl font-black text-[#4F39F6] ">DigiTool</a>
+            <div>
+                <ul className="sm:flex hidden gap-6 text-lg font-light justify-center items-center ">
+                    <li>Product</li>
+                    <li>Features</li>
+                    <li >Pricing</li>
+                    <li className='hidden lg:block'>Testimonials</li>
+                    <li className='hidden lg:block'>FAQ</li>
+                </ul>
+            </div>
+            <div className='flex gap-3 justify-center items-center'>
+                <div className="dropdown dropdown-end">
+                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+                        <div className="indicator">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /> </svg>
+                            <span className="badge badge-sm indicator-item">$</span>
+                        </div>
+                    </div>
+                    <div
+                        className="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow">
+                        <div className="card-body">
+                            <span className="text-lg font-bold">8 Items</span>
+                            <span className="text-info">Subtotal: $999</span>
+                            <div className="card-actions">
+                                <button className="btn btn-primary btn-block">View cart</button>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div className='font-black '>
+                    <p>Login</p>
+                </div>
+                <div >
+                    <button className="btn bg-[#4F39F6] btn-primary rounded-3xl">Get Started</button>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Navbar;
