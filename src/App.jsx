0af486553d7e6a46e602d96136825c25fae8c4a9
mahib-bit <sadmanmahib@gmail.com>
2026-06-07 00:46:24 +0600

    cards created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
 import Banner from './Components/Banner/Banner'
+import Products from './Components/Products/Products'
+import { Suspense } from "react";
 
 function App() {
 
+  const productsPromise = fetch('product.json')
+    .then(res => res.json())
 
   return (
     <>
@@ -14,9 +18,14 @@ function App() {
       <p className='text-center font-extralight'>Choose from our curated collection of premium digital products designed to boost your productivity and creativity.</p>
       <div className='flex gap-3 mx-auto justify-center items-center'>
         <button className='btn rounded-3xl bg-[#4F39F6] mt-5'>Products</button>
-      <button className='btn rounded-3xl text-[#4F39F6] border border-[#4F39F6] mt-5 bg-transparent'>Cart (0)</button>
+        <button className='btn rounded-3xl text-[#4F39F6] border border-[#4F39F6] mt-5 bg-transparent'>Cart (0)</button>
       </div>
-      </>
+
+      <Suspense fallback={<p>Loading...</p>}>
+        <Products productsPromise={productsPromise} />
+      </Suspense>
+
+    </>
   )
 }
 
---
+++ src/Components/Products/Products.jsx
@@ -0,0 +1,43 @@
+import { use } from "react";
+
+const Products = ({ productsPromise }) => {
+    const products = use(productsPromise);
+
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-10 max-w-7xl mx-auto">
+            {products.map((product) => (
+                <div key={product.id} 
+                className="border rounded-2xl p-6 border-[#05021d]">
+                    <div className="mb-4">
+                        <span className="px-3 py-1 rounded-full text-sm bg-[#100736] shadow-2xl">
+                            {product.tag}
+                        </span>
+                    </div>
+
+                    <h2 className="text-2xl font-semibold mb-2">{product.name}</h2>
+
+                    <p className="max-w-[290px] text-gray-500 mb-4">{product.description}</p>
+
+                    <h3 className="text-3xl font-bold mb-4">
+                        ${product.price}
+                        <span className="text-base font-normal text-gray-500">
+                            /{product.period}
+                        </span>
+                    </h3>
+
+                    <ul className="space-y-2 mb-6">
+                        {product.features.map((feature, index) => (
+                            <li key={index}><span className="font-bold text-green-500 mr-2">✓</span> {feature}</li>
+                        ))}
+                    </ul>
+
+                    <button className="btn w-full rounded-3xl bg-[#4F39F6] text-white">
+                        Buy Now
+                    </button>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default Products;
